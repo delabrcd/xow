9cac657ffd4a3cac330424fd4f0a6778e1d22055
delabrcd <delabrcd@clarkson.edu>
2023-02-20 19:04:39 -0500

    change request type

--- controller/UsbController.cpp
+++ controller/UsbController.cpp
@@ -3,21 +3,23 @@
 #include <memory>
 
 #define USB_MAX_BULK_TRANSFER_SIZE 32
-#define bmREQ_HID_OUT                           USB_SETUP_HOST_TO_DEVICE|USB_SETUP_TYPE_CLASS|USB_SETUP_RECIPIENT_INTERFACE
-#define HID_REQUEST_SET_REPORT                  0x09
+#define bmREQ_HID_OUT \
+    USB_SETUP_HOST_TO_DEVICE | USB_SETUP_TYPE_CLASS | USB_SETUP_RECIPIENT_INTERFACE
+#define HID_REQUEST_SET_REPORT 0x09
 
 UsbController::UsbController(std::unique_ptr<UsbDevice> usbDevice)
     : m_UsbDevice(std::move(usbDevice)), m_StopThreads(false) {
     GipDevice::SendPacket sendPacket =
         std::bind(&UsbController::sendClientPacket, this, std::placeholders::_1);
     m_Controller = std::make_unique<Controller>(sendPacket);
-    UsbDevice::ControlPacket pkt; 
-    uint8_t data[] = {0x01, 0x03, 0x01};
-    pkt.data = data;
+    UsbDevice::ControlPacket pkt;
+    uint8_t                  data[] = {0x01, 0x03, 0x02 + 4};
+
+    pkt.data    = data;
     pkt.request = HID_REQUEST_SET_REPORT;
-    pkt.value = 0x00; 
-    pkt.index = 0x00;
-    pkt.length = sizeof(data);
+    pkt.value   = 0x02;
+    pkt.index   = 0x00;
+    pkt.length  = sizeof(data);
     controlWrite(pkt, true);
     m_Threads.emplace_back(&UsbController::readBulkPackets, this,
                            m_UsbDevice->getEndpointInAddress());
